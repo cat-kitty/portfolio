6071b836b180cd74a3baeb017f338e0c1b71ee56
Kitty Cai <kittychoi15@gmail.com>
2024-10-13 20:03:00 -0400

    Good

---
+++ src/components/Review.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+export const Review = () => {
+    return (
+        <Box className="aboutsection" sx={{ margin: 10 }} id="about">
+            <div className="header">
+                <h1>Reviews</h1>
+            </div>
+        </Box >
+    )
+}
